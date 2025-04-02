8bf7efbb9d7e1784033360221742ded3781a15df
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 15:09:51 +0200

    Add getProperty and getIntProperty methods to return values of properties

--- OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
+++ OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
@@ -9,6 +9,7 @@ public class ConfigReader {
 	Properties properties;
 	private static final String CONFIG_FILE_PATH = ".\\src\\test\\resources\\config.properties";
 	
+	// Constructor to load the file
 	public ConfigReader() {
 		properties = new Properties();
 		try (FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH)) {
@@ -19,6 +20,16 @@ public class ConfigReader {
 		}
 	}
 	
+	// getProperty method read the properties
+	// return the value in String format
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 	
+	// getIntProperty method read the properties
+	// return the value in Int format
+	public int getIntProperty(String key) {
+		return Integer.parseInt(properties.getProperty(key));
+	}
 	
 }
