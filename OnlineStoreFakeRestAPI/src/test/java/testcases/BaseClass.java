8c1cf97b450c584c70b8063bb46cf3884f3b9d37
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 15:13:49 +0200

    Create a config reader object to read properties

--- OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
@@ -4,15 +4,20 @@ import org.testng.annotations.BeforeClass;
 
 import io.restassured.RestAssured;
 import routes.Routes;
+import utils.ConfigReader;
 
 public class BaseClass {
 
+	ConfigReader configReader;
+	
 	@BeforeClass
 	public void setup()
 	{
 		// This will avoid redundant calls of base URL in the requests
 		// Helps to focus on calling only the end points
 		RestAssured.baseURI = Routes.BASE_URL;
+		
+		configReader = new ConfigReader();
 	}
 	
 }
