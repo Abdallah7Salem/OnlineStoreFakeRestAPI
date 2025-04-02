827ececabafa3e91e7ed3128ebc2c4e937c3c62e
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 13:39:46 +0200

    Create productPayload method and random class object

--- OnlineStoreFakeRestAPI/src/test/java/payloads/Payload.java
+++ OnlineStoreFakeRestAPI/src/test/java/payloads/Payload.java
@@ -1,5 +1,6 @@
 package payloads;
 
+import java.util.Random;
 import com.github.javafaker.Faker;
 import pojo.Product;
 
@@ -8,9 +9,17 @@ public class Payload {
 	private static final Faker faker = new Faker();
 	private static final String categories[] = {"electronics", "furniture", "clothing", "books", "beauty"};
 	
+	Random random = new Random();
+	
 	// Product Payload
-	Product productPayload()
-
+	Product productPayload() {
+		String name = faker.commerce().productName();
+		Double price = Double.parseDouble(faker.commerce().price());
+		String description = faker.lorem().sentence();
+		String imageUrl = "https://i.pravatar.cc/100";
+		String category = categories[random.nextInt(5)];
+	
+	}
 	
 	// Cart Payload
 	
