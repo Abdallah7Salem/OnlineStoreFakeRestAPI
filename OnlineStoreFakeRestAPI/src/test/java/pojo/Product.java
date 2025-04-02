29005831d25851ec1ec4830f46867b5fa1891a26
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 13:16:11 +0200

    Add getters and setters for encapsulation

--- OnlineStoreFakeRestAPI/src/test/java/pojo/Product.java
+++ OnlineStoreFakeRestAPI/src/test/java/pojo/Product.java
@@ -9,4 +9,38 @@ public class Product {
 	private String category;
 	
 	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+	
 }
