cf75e6bb5db397ae910b8a24e926b417812ea4a1
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 14:49:27 +0200

    Create setup method and initialize base URL

--- OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
@@ -1,7 +1,18 @@
 package testcases;
 
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import routes.Routes;
+
 public class BaseClass {
 
-	
+	@BeforeClass
+	public void setup()
+	{
+		// This will avoid redundant calls of base URL in the requests
+		// Helps to focus on calling only the end points
+		RestAssured.baseURI = Routes.BASE_URL;
+	}
 	
 }
