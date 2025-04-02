3c64aece35af21db4b3609fe6357f88ad5e2a6f2
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 15:04:12 +0200

    Create the ConfigReader constructor to load the properties file

--- OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
+++ OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
@@ -1,11 +1,24 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
 
 	Properties properties;
 	private static final String CONFIG_FILE_PATH = ".\\src\\test\\resources\\config.properties";
+	
+	public ConfigReader() {
+		properties = new Properties();
+		try (FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH)) {
+			properties.load(fileInputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to load config.properties file");
+		}
+	}
+	
 	
 	
 }
