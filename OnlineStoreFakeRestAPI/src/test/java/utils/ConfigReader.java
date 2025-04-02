1d7628e4dca1e6e72a91d9a98bddaf5df8bdfaa8
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-02 15:00:34 +0200

    Store the properties file name in a variable => CONFIG_FILE_PATH

--- OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
+++ OnlineStoreFakeRestAPI/src/test/java/utils/ConfigReader.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 public class ConfigReader {
 
 	Properties properties;
-	
+	private static final String CONFIG_FILE_PATH = ".\\src\\test\\resources\\config.properties";
 	
 	
 }
