33f437b7a5d6ac03152ef225d1e488173797742a
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-04 13:33:12 +0200

    import the required packages for the test case

--- OnlineStoreFakeRestAPI/src/test/java/testcases/ProductTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/ProductTests.java
@@ -1,5 +1,22 @@
 package testcases;
 
+import pojo.Product;
+import routes.Routes;
+import utils.ConfigReader;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import payloads.Payload;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.List;
+
 public class ProductTests extends BaseClass {
 
+	
+	
+	
 }
