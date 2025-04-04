66ffd9fb79fed4889625d2e945aa2286f3fcc30a
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-04 16:24:58 +0200

    Import all required packages for product DDT class

--- OnlineStoreFakeRestAPI/src/test/java/testcases/ProductDataDrivenTest.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/ProductDataDrivenTest.java
@@ -1,8 +1,24 @@
 package testcases;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import pojo.Product;
+import routes.Routes;
+
 public class ProductDataDrivenTest extends BaseClass{
 
-	
+	@Test
+	public void testAddNewProduct()
+	{
+		
+	}
 	
 	
 	
