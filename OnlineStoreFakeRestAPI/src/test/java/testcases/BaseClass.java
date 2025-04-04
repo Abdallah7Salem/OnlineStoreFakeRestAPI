91e1f6f9155c40b0c163d6f1b3de9fc9737d85a3
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-04 15:24:54 +0200

    Add code to generate test logs

--- OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/BaseClass.java
@@ -1,10 +1,15 @@
 package testcases;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import org.testng.annotations.BeforeClass;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import routes.Routes;
 import utils.ConfigReader;
 
@@ -12,12 +17,23 @@ public class BaseClass {
 
 	ConfigReader configReader;
 	
+	RequestLoggingFilter requestLoggingFilter;
+	ResponseLoggingFilter responseLoggingFilter;
+	
 	@BeforeClass
-	public void setup()
+	public void setup() throws FileNotFoundException
 	{
 		// This will avoid redundant calls of base URL in the requests
 		// Helps to focus on calling only the end points
 		RestAssured.baseURI = Routes.BASE_URL;
+		
+		// Setup filters for logging
+		FileOutputStream fos = new FileOutputStream(".\\logs\\test_logging.txt");
+		PrintStream log = new PrintStream(fos, true);
+		requestLoggingFilter = new RequestLoggingFilter(log);
+		responseLoggingFilter = new ResponseLoggingFilter(log);
+		
+		RestAssured.filters(requestLoggingFilter, responseLoggingFilter);
 		
 		configReader = new ConfigReader();
 	}
