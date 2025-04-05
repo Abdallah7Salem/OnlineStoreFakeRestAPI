073a6061da1f7a3d28271e010505a37c1187ee9f
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-05 19:30:05 +0200

    Implement CartProduct POJO Class

---
+++ OnlineStoreFakeRestAPI/src/test/java/pojo/CartProduct.java
@@ -0,0 +1,32 @@
+package pojo;
+
+public class CartProduct {
+
+	private int productId;
+	private int quantity;
+	
+	// Constructor
+	public CartProduct(int productId, int quantity)
+	{
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
