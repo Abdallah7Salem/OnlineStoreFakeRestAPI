2eaecd74e69f875793f9a77d92718c90c7a8e25d
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-05 19:46:39 +0200

    Import all the required packages

--- OnlineStoreFakeRestAPI/src/test/java/testcases/CartTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/CartTests.java
@@ -1,7 +1,43 @@
 package testcases;
 
-public class CartTests {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.List;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import payloads.Payload;
+import pojo.Cart;
+import routes.Routes;
+
+public class CartTests extends BaseClass{
 
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
