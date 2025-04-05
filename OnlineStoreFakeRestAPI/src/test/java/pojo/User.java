743028dd573de57340c2184d95bcadaba09ba8b2
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-05 14:46:09 +0200

    Create a POJO class for the User Module

---
+++ OnlineStoreFakeRestAPI/src/test/java/pojo/User.java
@@ -0,0 +1,48 @@
+package pojo;
+
+/*
+{
+   id:20,
+   email:String,
+   username:String,
+   password:String,
+   name:{
+       firstname:String,
+       lastname:String
+       },
+   address:{
+   city:String,
+   street:String,
+   number:Number,
+   zipcode:String,
+   geolocation:{
+       lat:String,
+       long:String
+       }
+   },
+   phone:String
+}
+*/
+
+public class User {
+
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
