51d0dbe6c0423ec9f67f4b917ceed30f0e6fe262
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-06 15:37:21 +0200

    implement test method: testProductSchema

--- OnlineStoreFakeRestAPI/src/test/java/testcases/SchemaTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/SchemaTests.java
@@ -8,8 +8,22 @@ import org.testng.annotations.Test;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import routes.Routes;
 
-public class SchemaTests {
+public class SchemaTests extends BaseClass{
 
+	@Test
+	public void testProductSchema()
+	{
+		int productId=configReader.getIntProperty("productId");
+		
+		given()
+			.pathParam("id", productId)
+		
+		.when()
+			.get(Routes.GET_PRODUCT_BY_ID)
+		.then()
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("productSchema.json"));
+	}
+	
 	
 	
 }
