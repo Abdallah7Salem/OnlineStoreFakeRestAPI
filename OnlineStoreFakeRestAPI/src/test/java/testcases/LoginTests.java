2593369f3dffbdb9d6e98df9bf52aa5c0367d476
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-06 14:57:19 +0200

    Implement test method: testInvalidUserLogin

--- OnlineStoreFakeRestAPI/src/test/java/testcases/LoginTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/LoginTests.java
@@ -19,9 +19,34 @@ public class LoginTests extends BaseClass{
 	@Test
 	public void testInvalidUserLogin()
 	{
+		Login newLogin = Payload.loginPayload();
 		
+		given()
+			.contentType(ContentType.JSON)
+			.body(newLogin)
+		.when()
+			.post(Routes.AUTH_LOGIN)
+		.then()
+			.log().body()
+			.statusCode(401) // Expecting 401 for unauthorized access
+			.body(equalTo("username or password is incorrect")); // Validate the message response body
 	}
 	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
