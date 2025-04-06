2709fb44a47e6d3b20bf9b0267d05a0c74309023
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-06 14:46:26 +0200

    Add all required packages

--- OnlineStoreFakeRestAPI/src/test/java/testcases/LoginTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/LoginTests.java
@@ -1,8 +1,26 @@
 package testcases;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import payloads.Payload;
+import pojo.Login;
+import routes.Routes;
+
+
+import org.testng.annotations.Test;
+
 public class LoginTests extends BaseClass{
 
-	
+	@Test
+	public void testInvalidUserLogin()
+	{
+		
+	}
 	
 	
 	
