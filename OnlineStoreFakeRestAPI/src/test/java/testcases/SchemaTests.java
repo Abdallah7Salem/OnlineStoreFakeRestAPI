eabadd847e410cf53895c9a02bf655af05a68e8c
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-04-06 15:36:01 +0200

    import all the required packages

--- OnlineStoreFakeRestAPI/src/test/java/testcases/SchemaTests.java
+++ OnlineStoreFakeRestAPI/src/test/java/testcases/SchemaTests.java
@@ -1,5 +1,13 @@
 package testcases;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import routes.Routes;
+
 public class SchemaTests {
 
 	
